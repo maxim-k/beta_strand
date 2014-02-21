2a5c1a3ea9a71209a355ae52d5a6820659ce0618
maximk <massaraksh.x@gmail.com>
2014-02-21 12:53:38 +0400

    Predictation

--- naive_bayes.py
+++ naive_bayes.py
@@ -4,7 +4,6 @@ import os, math
 
 path = 'E:\\Science\\MG\Marat\\pairwise\\neib_pairwise'
 
-
 def get_strand_name(filename):
     '''
     Разделяет имя файла на части
@@ -53,25 +52,48 @@ def sum_strands(strand1, strand2):
     '''
     Складывает вероятности в двух стрендах
     '''
-    sum = {}
+    sum = dict()
     strands_keys = set(list(strand1.keys()) + list(strand2.keys()))
     for key in strands_keys:
-        sum[key] = strand1.get(key, 0) + strand2.get(key, 0)
+        if strand1 == {}:
+            sum[key] = strand2.get(key, 0)
+        else:
+            strand_keys_keys = set(list(strand1[key].keys()) + list(strand2[key].keys()))
+            sum_sum = dict()
+            for key_key in strand_keys_keys:
+                sum_sum[key_key] = strand1[key].get(key_key, 0) + strand2[key].get(key_key, 0)
+            sum[key] = sum_sum
     return sum
 
-def predict(line):
+def predict(line, prob):
     sheet = [[dict(), dict(), dict(), dict(), dict()],
              [dict(), dict(), dict(), dict(), dict()],
              [dict(), dict(), dict(), dict(), dict()]]
     pattern = [['-2L', '-1L', '0L', '1L', '2L'],
-               ['-2C', '-1C', '0C', '1C', '2С'],
+               ['-2C', '-1C', '0C', '1C', '2C'],
                ['-2R', '-1R', '0R', '1R', '2R']]
-    return None
+    pos = 2
+    for aa in line:
+        for cur in range(pos-2, pos+3):
+            sheet[0][cur] = sum_strands(sheet[0][cur], prob[pattern[0][cur+2-pos]])
+            if cur != pos:
+                sheet[1][cur] = sum_strands(sheet[1][cur], prob[pattern[1][cur+2-pos]])
+            sheet[2][cur] = sum_strands(sheet[2][cur], prob[pattern[2][cur+2-pos]])
+        sheet[0].append(dict())
+        sheet[1].append(dict())
+        sheet[2].append(dict())
+        pos += 1
+    return sheet
 
+prob = dict()
 for dirname, dirnames, filenames in os.walk(path):
     for filename in filenames:
         file = open(os.path.join(dirname, filename), 'r').read()
         strand_name = get_strand_name(filename)
-
         aa_norm = set_normal(file)
-        n = set_max(sum_strands(aa_norm['A'], aa_norm['Y']))
+        prob[strand_name[2]] = set_normal(file)
+        #n = set_max(sum_strands(aa_norm['A'], aa_norm['Y']))
+prob['0C'] = dict()
+
+p = predict('RVXAALPYY', prob)
+print()
